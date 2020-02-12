761ab5f6456aa10477c4f448df92ec86b0ff5976
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-02-12 19:08:01 +0200

    Add invoice report module.

---
+++ l10n_ro_invoice_report/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2015 Deltatech
+# Copyright 2018 OdooERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Romania - Invoice Report",
+    "summary": "Romania - Invoice Report",
+    "version": "13.0.1.0.0",
+    "category": "Localization",
+    "author": "Deltatech," "OdooERP Romania," "Odoo Community Association (OCA)",
+    "website": "https://odoo-community.org",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["base", "account", "l10n_ro_partner_create_by_vat"],
+    "data": [
+        "views/report_templates.xml",
+        "views/account_invoice_view.xml",
+        "views/res_bank_view.xml",
+        "views/invoice_report.xml",
+        "views/res_config_settings_view.xml",
+    ],
+}
---
+++ l10n_ro_invoice_report/models/res_config_settings.py
@@ -0,0 +1,12 @@
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    invoice_no_signature_text = fields.Text(
+        related="company_id.invoice_no_signature_text", readonly=False
+    )
